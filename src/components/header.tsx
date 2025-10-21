3c023c4a2e80239ab25f08201597abd7ff06500c
최민혁 <130155327+lactofreee@users.noreply.github.com>
2025-10-21 21:34:57 +0900

    [Feat] header 바 로그아웃 버튼 개선

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,12 +21,14 @@ export function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 md:px-6">
       <div className="container flex h-14 items-center">
         <MobileSidebar />
         <div className="mr-4 hidden md:flex">
           <div className="mr-6 flex items-center space-x-2">
-            <span className="hidden font-bold sm:inline-block">Pull-Mate</span>
+            <span className="hidden font-bold sm:inline-block lg:text-xl">
+              Pull-Mate
+            </span>
           </div>
         </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
